4c7c616d6a5c16d55440c45670f7a917b81bd7bb
Mustafa <aweel017@gmail.com>
2021-07-10 19:59:48 +0400

    bug fix

--- resources/user_landing.py
+++ resources/user_landing.py
@@ -8,10 +8,10 @@ class UserLanding(Resource):
         # users_ref: List = db.collection("users").where("uid", "==", user_id).get()
         user_ref = db.collection("users").document(user_id).get()
 
-        if not user_ref:
-            return {"message": f"user with ID {user_id} was not found"}, 400
-
         user_document = user_ref.to_dict()
+
+        if not user_document:
+            return {"message": f"user with ID {user_id} was not found"}, 400
 
         if user_document.get("userType") == "baqala":
             return user_document, 200
